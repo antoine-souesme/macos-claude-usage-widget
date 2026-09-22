50b7f168f611ccc31f33573c956a981c98b0e9d4
Antoine Souesme <antoine.souesme@gmail.com>
2026-09-22 13:22:36 +0200

    Version 1.0.2

--- Sources/ClaudeUsageCore/Version.swift
+++ Sources/ClaudeUsageCore/Version.swift
@@ -2,4 +2,4 @@
 ///
 /// Le script de construction le recopie dans l'`Info.plist` du bundle, et le
 /// workflow de release vérifie que le tag poussé correspond à cette valeur.
-public let appVersion = "1.0.1"
+public let appVersion = "1.0.2"
